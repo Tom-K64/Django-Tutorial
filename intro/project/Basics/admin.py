b9eaa5563ae618a0aa8dba39926eec0280f0c47e
Kanhaya Kataruka <kanhaya.code@gmail.com>
2023-07-27 19:08:28 +0530

    Models and basic ORM

--- intro/project/Basics/admin.py
+++ intro/project/Basics/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import StudentDetailsModel
 # Register your models here.
+
+admin.site.register(StudentDetailsModel)
--- intro/project/Basics/models.py
+++ intro/project/Basics/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudentDetailsModel(models.Model):
+    name=models.CharField(max_length=100)
+    phone_number=models.CharField(max_length=15)
+    gender=models.CharField(max_length=10)
+    
