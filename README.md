34339954f987a9a3c1cf3f68615033fd9139f871
Kanhaya Kataruka <kanhaya.code@gmail.com>
2023-07-28 21:21:49 +0530

    ORMs Commands

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Django-Tutorial
+->Django Set up
+->File structure
+->Creation of app
+->Basic API (first api)
+->Models and fields
+->ORMs  -> all(),get(),create(),save(),filter(),delete(),count()
--- intro/project/Basics/models.py
+++ intro/project/Basics/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 class StudentDetailsModel(models.Model):
-    name=models.CharField(max_length=100)
-    phone_number=models.CharField(max_length=15)
-    gender=models.CharField(max_length=10)
-    
+    name=models.CharField(max_length=100,null=True, blank=True)
+    phone_number=models.CharField(max_length=15,null=True, blank=True)
+    gender=models.CharField(max_length=10,null=True, blank=True)
+    standard=models.IntegerField(null=True, blank=True)
+
+
+"""
+anish
+sahil
+kanhaya
+riya
+
+"""
